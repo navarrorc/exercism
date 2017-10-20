daf345a28f9d6e3d1e0f802c985ef651a82ece12
Roberto C Navarro <rnavarro70@gmail.com>
2017-10-20 16:33:03 -0500

    complete Leap

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,14 +4,24 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        if (year % 4 == 0)
-        {
-            return true;
-        }
-        else
+        if (year % 4 != 0)
         {
             return false;
         }
-        //throw new NotImplementedException("You need to implement this function.");
+        if (year % 4 == 0 && year % 100 != 0)
+        {
+            return true;
+        }
+        if (year % 100 == 0 && year % 400 != 0)
+        {
+            return false;
+        }
+        if (year % 400 == 0)
+        {
+            return true;
+        }
+
+        return false;
+
     }
 }

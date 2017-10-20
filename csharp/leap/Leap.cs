cdec411ca0424c5523fe94359b9d1b65124e6948
Roberto C Navarro <rnavarro70@gmail.com>
2017-10-20 16:13:58 -0500

    Initial Commit

---
+++ csharp/leap/Leap.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+        if (year % 4 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        //throw new NotImplementedException("You need to implement this function.");
+    }
+}
